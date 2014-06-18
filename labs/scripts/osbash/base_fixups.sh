cb6b1470f9c5a78a50eaffa93f174616458f763a
Roger Luethi <rl@patchworkscience.org>
2014-06-18 10:29:16 +0200

    Add osbash helper scripts
    This changeset adds two simple helper scripts.
    
    base_fixups.sh is run on every basedisk while it is built. The script
    can be used to fix whatever is necessary to make the basedisk work.
    Right now, all it does is remove 70-persistent-net.rules which (if left
    untouched) would result in a network interface order that would differ
    from Vagrant's.
    
    The second script is a trivial debug helper that does nothing but wait
    for the removal of a file it creates in /tmp. When the file is removed,
    execution continues.
    
    Partial-Bug: 1312764
    Implements: blueprint openstack-training-labs
    Change-Id: Id75a89ced5dcf132e91e7426df15436de585a203

---
+++ labs/scripts/osbash/base_fixups.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+source "$TOP_DIR/config/paths"
+source "$LIB_DIR/functions.guest"
+
+indicate_current_auto
+
+exec_logfile
+
+# This script is run by vm_install_base. It should work on all supported
+# distributions.
+
+# match Vagrant which removes 70-persistent-net.rules (so we get the same
+# names for our network interfaces)
+sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
---
+++ labs/scripts/osbash/wait_debug.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+source "$TOP_DIR/config/paths"
+source "$LIB_DIR/functions.guest"
+
+indicate_current_auto
+
+exec_logfile
+
+# Wait for removal of /tmp/remove_to_continue
+wait_for_file
