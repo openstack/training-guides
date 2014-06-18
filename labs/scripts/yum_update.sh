23098a88717d03c5c88d050d86f745c014edb442
Roger Luethi <rl@patchworkscience.org>
2014-06-18 10:04:18 +0200

    Scripts to configure yum and upgrade system
    The first script in this changeset configures yum to use a proxy (if
    VM_PROXY is set) and adds the RDO repo.
    
    The second script updates the packages installed in a yum-based system.
    
    Partial-Bug: 1312764
    Implements: blueprint openstack-training-labs
    Change-Id: Iad42b6aad2bf48329cb2c3bb2bc7a1d698d9c608

---
+++ labs/scripts/yum_update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+source "$TOP_DIR/config/paths"
+source "$CONFIG_DIR/openstack"
+source "$LIB_DIR/functions.guest"
+
+indicate_current_auto
+
+exec_logfile
+
+# Upgrade installed packages
+sudo yum -y update
