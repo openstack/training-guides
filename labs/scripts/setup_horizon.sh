5a32910539a72ed74e21e3b7fbbf4cb60b287d5b
Pranav Salunke <dguitarbite@gmail.com>
2014-08-10 13:37:19 +0530

    Adds horizon scripts for training labs
    Adds horizon scripts for training labs which will install
    OpenStack dashboard.
    
    Change-Id: I824c5571410411f2f17671657c90d463c2846ee2
    Partial-Bug: 1312764
    Implements: blueprint openstack-training-labs

---
+++ labs/scripts/setup_horizon.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+source "$TOP_DIR/config/paths"
+source "$LIB_DIR/functions.guest"
+exec_logfile
+
+indicate_current_auto
+
+#------------------------------------------------------------------------------
+# Set up OpenStack Dashboard (horizon).
+#------------------------------------------------------------------------------
+
+echo "Installing horizon."
+sudo apt-get install -y openstack-dashboard memcached
+
+echo "Purging Ubuntu theme."
+sudo dpkg --purge openstack-dashboard-ubuntu-theme
+
+echo "Reloading apache and memcached service."
+sudo service apache2 restart
+sudo service memcached restart
