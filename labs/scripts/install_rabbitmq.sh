8d08b00899bc9e405026a9b22991263408254b94
Roger Luethi <rl@patchworkscience.org>
2014-08-11 11:41:50 +0200

    labs: install RabbitMQ message broker service
    This changeset installs RabbitMQ on the controller node.
    
    The password needed by OpenStack services to use rabbitmq can be sourced
    from config/credentials.
    
    Implements: blueprint openstack-training-labs
    Change-Id: Ifff79e7d84b2f02c2bdded854721a6a73fa82465

---
+++ labs/scripts/install_rabbitmq.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+source "$TOP_DIR/config/paths"
+source "$CONFIG_DIR/credentials"
+source "$LIB_DIR/functions.guest"
+
+exec_logfile
+
+indicate_current_auto
+
+#-------------------------------------------------------------------------------
+# Install the message broker service (RabbitMQ).
+
+echo "Installing RabbitMQ."
+sudo apt-get install -y rabbitmq-server
+
+echo "Setting RabbitMQ password to '$RABBIT_PASSWORD'."
+sudo rabbitmqctl change_password guest "$RABBIT_PASSWORD"
