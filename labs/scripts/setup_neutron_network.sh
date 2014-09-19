35681c311b1178588f3fec384b6f61cd63753719
Roger Luethi <rl@patchworkscience.org>
2014-09-19 14:27:07 +0200

    labs: remove redundant restart for ovs agent
    Setup_neutron_network.sh restarts neutron-plugin-openvswitch-agent twice.
    The second instance does not seem necessary and is not documented in
    the install-guide, either.
    
    Change-Id: I0cd1d64c94fd64130c530e761879515b9368c7b4

--- labs/scripts/setup_neutron_network.sh
+++ labs/scripts/setup_neutron_network.sh
@@ -138,5 +138,3 @@ sudo service neutron-l3-agent restart
 sudo service neutron-dhcp-agent restart
 sudo service neutron-metadata-agent restart
 
-echo "Restarting the OVS agent."
-sudo service neutron-plugin-openvswitch-agent restart
