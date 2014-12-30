3a3914e8f3e3e997c6f740b4a70588e56547482e
Roger Luethi <rl@patchworkscience.org>
2014-12-30 16:39:56 +0100

    Reorder neutron_network to match Juno
    Reorder neutron_network to match the Juno install-guide. No functional
    changes.
    
    Change-Id: I5d376fc3d2ce25485a60ca45e9ce198d6fb4ec9c

--- labs/scripts/ubuntu/setup_neutron_network.sh
+++ labs/scripts/ubuntu/setup_neutron_network.sh
@@ -11,7 +11,7 @@ indicate_current_auto
 
 #------------------------------------------------------------------------------
 # Set up OpenStack Networking (neutron) for network node.
-# http://docs.openstack.org/icehouse/install-guide/install/apt/content/neutron-ml2-network-node.html
+# http://docs.openstack.org/juno/install-guide/install/apt/content/neutron-network-node.html
 #------------------------------------------------------------------------------
 
 echo "Editing /etc/sysctl.conf: enable IP forwarding, disable RPF filter."
@@ -38,6 +38,11 @@ neutron_admin_password=$(service_to_user_password neutron)
 conf=/etc/neutron/neutron.conf
 echo "Configuring $conf."
 
+# Configure AMQP parameters
+iniset_sudo $conf DEFAULT rpc_backend neutron.openstack.common.rpc.impl_kombu
+iniset_sudo $conf DEFAULT rabbit_host controller-mgmt
+iniset_sudo $conf DEFAULT rabbit_password "$RABBIT_PASSWORD"
+
 # Configuring [DEFAULT] section
 iniset_sudo $conf DEFAULT auth_strategy keystone
 
@@ -50,17 +55,32 @@ iniset_sudo $conf keystone_authtoken admin_tenant_name "$SERVICE_TENANT_NAME"
 iniset_sudo $conf keystone_authtoken admin_user "$neutron_admin_user"
 iniset_sudo $conf keystone_authtoken admin_password "$neutron_admin_password"
 
-# Configure AMQP parameters
-iniset_sudo $conf DEFAULT rpc_backend neutron.openstack.common.rpc.impl_kombu
-iniset_sudo $conf DEFAULT rabbit_host controller-mgmt
-iniset_sudo $conf DEFAULT rabbit_password "$RABBIT_PASSWORD"
-
 # Configure network plugin parameters
 iniset_sudo $conf DEFAULT core_plugin ml2
 iniset_sudo $conf DEFAULT service_plugins router
 iniset_sudo $conf DEFAULT allow_overlapping_ips True
 
 iniset_sudo $conf DEFAULT verbose True
+
+echo "Configuring the OVS plug-in to use GRE tunneling."
+conf=/etc/neutron/plugins/ml2/ml2_conf.ini
+
+# Under the ml2 section
+iniset_sudo $conf ml2 type_drivers gre
+iniset_sudo $conf ml2 tenant_network_types gre
+iniset_sudo $conf ml2 mechanism_drivers openvswitch
+
+# Under the ml2_type_gre section
+iniset_sudo $conf ml2_type_gre tunnel_id_ranges 1:1000
+
+# Under the securitygroup section
+iniset_sudo $conf securitygroup enable_security_group True
+iniset_sudo $conf securitygroup firewall_driver neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
+
+# Under the ovs section
+iniset_sudo $conf ovs local_ip "$(hostname_to_ip network-data)"
+iniset_sudo $conf ovs tunnel_type gre
+iniset_sudo $conf ovs enable_tunneling True
 
 echo "Configuring Layer-3 agent."
 conf=/etc/neutron/l3_agent.ini
@@ -113,26 +133,6 @@ iniset_sudo $conf DEFAULT verbose True
 # The next two steps according to the install-guide (configuring
 # service_neutron_metadata_proxy and neutron_metadata_proxy_shared_secret)
 # are done in setup_neutron_controller.sh.
-
-echo "Configuring the OVS plug-in to use GRE tunneling."
-conf=/etc/neutron/plugins/ml2/ml2_conf.ini
-
-# Under the ml2 section
-iniset_sudo $conf ml2 type_drivers gre
-iniset_sudo $conf ml2 tenant_network_types gre
-iniset_sudo $conf ml2 mechanism_drivers openvswitch
-
-# Under the ml2_type_gre section
-iniset_sudo $conf ml2_type_gre tunnel_id_ranges 1:1000
-
-# Under the ovs section
-iniset_sudo $conf ovs local_ip "$(hostname_to_ip network-data)"
-iniset_sudo $conf ovs tunnel_type gre
-iniset_sudo $conf ovs enable_tunneling True
-
-# Under the securitygroup section
-iniset_sudo $conf securitygroup firewall_driver neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
-iniset_sudo $conf securitygroup enable_security_group True
 
 echo "Restarting the Open vSwitch (OVS) service."
 sudo service openvswitch-switch restart
