60cc16534091173788f564054e8f535af51e61af
Roger Luethi <rl@patchworkscience.org>
2015-01-02 16:34:06 +0100

    Reorder external_network to match Juno
    Reorder external_network to match the Juno install-guide. No functional
    changes.
    
    Change-Id: Ie41983f2f70a7edad42ad7c65ed22e838551d646

--- labs/scripts/config_external_network.sh
+++ labs/scripts/config_external_network.sh
@@ -4,15 +4,17 @@ TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
 source "$TOP_DIR/config/paths"
 source "$CONFIG_DIR/credentials"
 source "$LIB_DIR/functions.guest"
-source "$CONFIG_DIR/admin-openstackrc.sh"
 exec_logfile
 
 indicate_current_auto
 
 #------------------------------------------------------------------------------
 # Create the external network and a subnet on it
-# http://docs.openstack.org/icehouse/install-guide/install/apt/content/neutron_initial-external-network.html
+# http://docs.openstack.org/juno/install-guide/install/apt/content/neutron_initial-external-network.html
 #------------------------------------------------------------------------------
+
+echo "Sourcing the admin credentials."
+source "$CONFIG_DIR/admin-openstackrc.sh"
 
 echo "Waiting for neutron to start."
 until neutron net-list >/dev/null 2>&1; do
