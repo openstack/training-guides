c992ae3e218f4e898a8de45e9d18b386392bf524
Roger Luethi <rl@patchworkscience.org>
2015-04-11 09:16:16 +0200

    labs: Automatically download install ISO in Windows
    With this patch, the Windows batch scripts attempt to automatically
    download the install ISO image (this is already the case on Linux, OS X).
    
    Change-Id: I080b593f2b627801845f18587521ad64d53b3504

---
+++ labs/wbatch/downloader.js
@@ -0,0 +1,12 @@
+/* Taken from http://superuser.com/a/536400 */
+/* Use: cscript /nologo downloader.js <URL> */
+
+var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false);
+WinHttpReq.Send();
+
+BinStream = new ActiveXObject("ADODB.Stream");
+BinStream.Type = 1;
+BinStream.Open();
+BinStream.Write(WinHttpReq.ResponseBody);
+BinStream.SaveToFile("downloaded.bin");
