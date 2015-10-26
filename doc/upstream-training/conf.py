1d652fcc3dc44678f0017e5d436a034afa12d259
Andreas Jaeger <aj@suse.de>
2015-10-26 22:00:23 +0100

    Do not copy sources
    We do not need to copy source files anymore with current
    openstackdocstheme, unset html_show_sourcelink and html_copy_source
    links. Report a bug works without these as well.
    
    This reduces the amount of files we build and publish (for Install Guide
    from 42 MB to 39 MB), and thus speeds up building (from 44 to 41s for
    Install Guide) slightly.
    
    Change-Id: I666caaddf864876fc85145381d995297008b0e9a

--- doc/upstream-training/conf.py
+++ doc/upstream-training/conf.py
@@ -178,8 +178,7 @@ html_use_index = False
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-# This one is needed for "Report a bug".
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 # html_show_sphinx = True
@@ -198,6 +197,8 @@ html_use_index = False
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'upstream-training'
 
+# If true, publish source files
+html_copy_source = False
 
 # -- Options for LaTeX output ---------------------------------------------
 
