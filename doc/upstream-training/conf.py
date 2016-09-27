c77f740043146d87bcddce8de71834b3c4ff3abe
Akihiro Motoki <amotoki@gmail.com>
2016-09-28 04:39:40 +0900

    Show TOC in side bar
    The current sidebar only has a link "Contents" and
    provides almost no information.
    It looks reasonable to show TOC in the sidebar.
    openstackdocstheme 1.5.0 has an option 'sidebar_mode'
    and it allows us to show TOC in the sidebar.
    
    Change-Id: I97b7f4d67053bf065ae0035acf7b973b9450eeb2

--- doc/upstream-training/conf.py
+++ doc/upstream-training/conf.py
@@ -120,7 +120,9 @@ html_theme = 'openstackdocs'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'sidebar_mode': 'toc',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [openstackdocstheme.get_html_theme_path()]
